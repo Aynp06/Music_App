1dbfb82c24e0780ac197613fb856e40ffe77bf4c
aynp06 <ayushmanepal17@gmail.com>
2024-12-16 21:51:49 +0545

    Dark mode setting added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,11 +15,8 @@ class _HomepageState extends State<Homepage> {
       body: const Center(child: Text("Welcome to MusicMate")),
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        shadowColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 5,
-        title: const Center(child: Text("P L A Y L I S T")),
-      ),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: const Text("P L A Y L I S T")),
       drawer: const Mydrawer(),
     );
   }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:musicmate/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -6,12 +9,31 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("S E T T I N G "),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        shadowColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 5,
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("S E T T I N G S"),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(15)),
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Dark Mode",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+              ),
+              CupertinoSwitch(
+                  value: Provider.of<ThemeProvider>(context, listen: false)
+                      .isDarkMode,
+                  onChanged: (value) =>
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleTheme())
+            ],
+          ),
+        ));
   }
 }
